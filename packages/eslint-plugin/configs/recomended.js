fee5fa4e2c3c4a2cc1c0096a21dbf3397d78fd97
Dmytro Maretskyi <maretskii@gmail.com>
2020-09-08 15:07:49 +0200

    Add eslint plugin resoultion workaround

--- packages/eslint-plugin/configs/recomended.js
+++ packages/eslint-plugin/configs/recomended.js
@@ -2,6 +2,9 @@
 // Copyright 2020 DXOS.org
 //
 
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
+require('@rushstack/eslint-config/patch/modern-module-resolution');
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
