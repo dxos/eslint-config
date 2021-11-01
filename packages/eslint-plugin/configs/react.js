338ce0bc277b4cfc0d0a3eb0154405aecf4311ff
Josiah Witt <josiah@witt.life>
2021-11-01 19:55:08 -0400

    feat: add react jsx linting

--- packages/eslint-plugin/configs/react.js
+++ packages/eslint-plugin/configs/react.js
@@ -15,6 +15,29 @@ module.exports = {
     ],
     'no-use-before-define': 'off',
     'react/display-name': 'off',
+    'react/function-component-definition': ['error', {
+      'namedComponents': 'arrow-function',
+      'unnamedComponents': 'arrow-function'
+    }],
+    'react/jsx-first-prop-new-line': [
+      'error',
+      'multiline-multiprop'
+    ],
+    'react/jsx-tag-spacing': ['error', {
+      'closingSlash': 'never',
+      'beforeSelfClosing': 'always',
+      'afterOpening': 'never',
+      'beforeClosing': 'never'
+    }],
+    'react/jsx-wrap-multilines': ['error', {
+      'declaration': 'parens-new-line',
+      'assignment': 'parens-new-line',
+      'return': 'parens-new-line',
+      'arrow': 'parens-new-line',
+      'condition': 'parens-new-line',
+      'logical': 'parens-new-line',
+      'prop': 'parens-new-line'
+    }],
     'react/prop-types': 'off'
   },
   settings: {
