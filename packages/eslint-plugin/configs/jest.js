10da831dfc8940021936b4ff38b404b5a21a0d00
Josiah Witt <josiah@witt.life>
2021-11-04 05:22:50 -0400

    Comment linting improvements (#7)
    * refactor: separate jest config from react & recommended
    
    * feat: improve comments linting

--- packages/eslint-plugin/configs/jest.js
+++ packages/eslint-plugin/configs/jest.js
@@ -1,14 +1,10 @@
 const recomended = require('./recomended');
 
 module.exports = {
-  ...recomended,
   extends: [
-    ...recomended.extends,
     'plugin:jest/recommended',
-    'plugin:react/recommended'
   ],
   rules: {
-    ...recomended.rules,
     'jest/valid-expect': 'off'
   }
 }
